23ca43bd9fbd71fdf0d2f2773877d484562baf89
thbins <joons0218@gmail.com>
2025-11-13 03:06:56 +0900

    BFS 탐색 알고리즘 구현

---
+++ bfs.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+# 그래프 구조
+# 
+#         A
+#       / | \
+#      B  D  E
+#     / \
+#    C   D
+
+graph = {
+    'A': ['B', 'D', 'E'],
+    'B': ['A', 'C', 'D'],
+    'C': ['B'],
+    'D': ['A', 'B'],
+    'E': ['A']
+}
+
+def bfs(graph, start_v):
+    visited = [start_v]
+    queue = deque(start_v)
+    while queue:
+        cur_v = queue.popleft()
+        for v in graph[cur_v]:
+            if v not in visited:
+                visited.append(v)
+                queue.append(v)
+    return visited
+
+print(bfs(graph, 'A'))
